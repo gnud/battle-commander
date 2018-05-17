5cac72efdac4c14a0daa058d2a14c504ba1dee04
damjan_dimitrioski <damjandimitrioski@gmail.com>
2018-05-17 11:04:39 +0200

    - Ships models  * Randomization  * Model specific data - Board  * reveal ship character  * check and place point on grid methods  * impletend cheat reveal functionality  * player hit implemented

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import random
 import re
 import string
 import sys
@@ -7,18 +8,110 @@ horizontal_letters = range(1, 11)
 vertical_letters = string.ascii_uppercase
 
 
+class Ship:
+    def __init__(self, board):
+        self.board = board
+
+        self.x = 0
+        self.y = 0
+
+        self.blocks_data = {}
+
+        self.randomize()
+
+    def randomize(self):
+        def fill_data():
+            block_available = row[random_y:random_y + self.blocks]
+            for c in block_available:
+                if c[1]:
+                    break
+            else:
+                self.blocks_data = { b[0]: True for b in block_available}
+                print(self.blocks_data)
+
+                return True
+
+        for i in range(0, self.board.Y):
+            row = self.board.find_rows_with_n_free(vertical_letters[i])
+
+            random_y = random.randrange(self.board.Y)
+
+            if random_y <= self.board.X / 2:
+                if not self.blocks + random_y > self.board.X:
+                    if fill_data():
+                        break
+
+            if random_y > self.board.X / 2:
+                if not self.blocks <= (self.board.X / 2):
+                    if fill_data():
+                        break
+
+        for c in self.blocks_data.keys():
+            self.board.occupy_spot(c)
+
+        print(self.blocks_data.keys())
+
+    def check(self, point):
+        try:
+            if (point in self.blocks_data):
+                return True
+        except:
+            return False
+
+    def get_position(self):
+        return self.x, self.y,
+
+
+class Battle(Ship):
+    def __init__(self, board):
+        self.blocks = 5
+        self.name = 'Battle ship'
+        self.board = board
+
+        super(Battle, self).__init__(board)
+
+
+class Destroyer(Ship):
+    def __init__(self, board):
+        self.blocks = 4
+        self.name = 'Destroyer ship'
+        self.board = board
+
+        super(Destroyer, self).__init__(board)
+
+
 class Board:
     def __init__(self):
         self.is_labels = True
         self.alphabet = vertical_letters
         self.nums = horizontal_letters
         self.cloaked_symbol = "."
+        self.ship_char = '='
         self.X = 10
         self.Y = 10
+        self.board_map = {}
 
         self.board = []
 
         self.build_board()
+
+        self.ships = [
+            Battle(self),
+            Destroyer(self)
+        ]
+
+    def is_ship_in_sight(self, point):
+        if(self.board_map[point]):
+            return True
+        return False
+
+    def all_free_spots(self, label):
+        is_free = len([l for l in self.board_map.keys() if l[0] == label]) > 0
+
+        if is_free:
+            return is_free
+
+        a = [s.blocks_data for s in self.ships]
 
     def reset(self):
         pass
@@ -27,14 +120,36 @@ class Board:
         return self.X * self.Y
 
     def build_board(self):
+        self.board_map = {"%s%s" % (v, h): False for v in vertical_letters[:10] for h in horizontal_letters}
+
         for x in range(self.X):
             self.board.append([self.cloaked_symbol] * self.Y)
 
-    def render(self):
+    def occupy_spot(self, area):
+        self.board_map[area] = True
+
+    def is_spot_free(self, spot):
+        if self.board_map[spot[0].upper()]:
+            return True
+        return False
+
+    def find_rows_with_n_free(self, n):
+        row = [(key, value) for key, value in self.board_map.items() if n in key.upper()]
+
+        return row
+
+    def render(self, cheat=False):
         self.render_h_labels()
 
         for idx, row in enumerate(self.board):
             self.render_v_labels(idx)
+
+            if cheat:
+                for idx_c, c in enumerate(row):
+                    x, y = idx_c, idx
+                    if self.is_ship_in_sight('%s%s' %(self.alphabet[idx], idx_c + 1)):
+                        row[idx_c] = self.ship_char
+
             print(" ".join(row))
 
     def render_v_labels(self, cur_letter):
@@ -54,13 +169,28 @@ class Board:
     def reveal(self):
         """Show ship position command - aids in debugging"""
 
-        print('Show me magic...')
+        self.render(True)
 
     def construct_battleship(self):
         sqrs = 5
 
     def construct_destoyer(self):
         sqrs = 4
+
+    def char_to_position(self, char):
+        return vertical_letters.index(char.upper())
+
+    def hit(self, position):
+        letter, x_pos = position
+
+        label = '%s%s' % (letter, x_pos)
+
+        is_hit = self.is_spot_free([label])
+
+        if is_hit:
+            print('bang!')
+        if not is_hit:
+            print('Missed! - Sorry.')
 
 
 class Player:
@@ -182,7 +312,8 @@ class Game:
         # TODO: print stats
 
     def process_move(self, moves):
-        print('Doing action stub...')
+        self.board.hit(moves)
+
 
     def parse_cmd(self, val):
         result = self.menu.parse_input(val)
